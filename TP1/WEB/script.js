c5fad79ba42ca230838010652367ce60186f200e
COIN COIN <112950422+lorensviguie@users.noreply.github.com>
2025-11-04 14:20:24 +0100

    Update script.js

--- TP1/WEB/script.js
+++ TP1/WEB/script.js
@@ -4,7 +4,7 @@ const responseDiv = document.getElementById('response');
 button.addEventListener('click', async () => {
     responseDiv.textContent = "Chargement...";
     try {
-        const res = await fetch('https://api-test-mgmt.azure-api.net/hello'); // Remplace par l'URL de ton API
+        const res = await fetch('http://api-test-mgmt.azure-api.net/hello'); // Remplace par l'URL de ton API
         if (!res.ok) throw new Error("Erreur lors de l'appel API");
         const data = await res.json();
         responseDiv.textContent = `Réponse API: ${JSON.stringify(data)}`;
